9a9d8aa634ca80aeb8cfd00bfc1f262ad25b12cb
Alex Blackburn <alex.blackburn1995@gmail.com>
2022-05-13 18:37:45 -0400

    commit tree d8af38cf4452d1e1fccd9068ad3fab7d41445024

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+const main = document.getElementById("main")
+main.remove("main")
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+newHeader.innerHTML = "Alex is the champion!"
+newHeader.className = "victory"
+
+
+
+
